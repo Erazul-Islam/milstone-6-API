a432d12f468a4b7cb6a58268b892cbca40144f1f
Erazul Islam <erazul89@gmail.com>
2023-08-27 15:45:55 +0600

    added

--- api.js
+++ api.js
@@ -9,22 +9,23 @@
 // console.log(user)
 // console.log(stringified)
 
-const user = {
-    id: 1,
-    name : 'Avd',
-    job : 'developer',
-    address : {
-        street : 'Dairy gate',
-        city : 'Dhaka',
-        country : "bd"
-    },
-    products : ['laptop','mic'],
-    revenue : 23000,
-    isOpen : true,
-    isNew : false,
-}
-console.log(user);
-const userJson = JSON.stringify(user);
-console.log(userJson);
-const shopObj = JSON.parse(userJson);
-console.log( typeof userJson)
+// const user = {
+//     id: 1,
+//     name : 'Avd',
+//     job : 'developer',
+//     address : {
+//         street : 'Dairy gate',
+//         city : 'Dhaka',
+//         country : "bd"
+//     },
+//     products : ['laptop','mic'],
+//     revenue : 23000,
+//     isOpen : true,
+//     isNew : false,
+// }
+// console.log(user);
+// const userJson = JSON.stringify(user);
+// console.log(userJson);
+// const shopObj = JSON.parse(userJson);
+// console.log( typeof userJson)
+
